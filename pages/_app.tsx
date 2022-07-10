c79b88562972edf3f040e778bf47f5791d3f05a6
nitin reddy <reddynitin3@gmail.com>
2022-07-10 20:03:58 +0530

    Added dev tool icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp ( { Component, pageProps }: AppProps )
     <ThemeProvider attribute="class" enableSystem={ false }>
       <Navigation />
       <Component { ...pageProps } />
-      {/* <Footer /> */ }
+      <Footer />
     </ThemeProvider>
   );
 }

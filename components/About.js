e7f535b17b722e93aa017569318daec015663e81
nitin reddy <reddynitin3@gmail.com>
2022-07-10 20:26:35 +0530

    Refactored the social icons component

--- components/About.js
+++ components/About.js
@@ -8,6 +8,23 @@ const About = () => {
   return (
     <div className="container px-4 mx-auto">
       <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
+
+        <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
+          <Image
+            src={profile}
+            alt="Profile"
+            priority={true}
+            className="rounded-lg"
+            width={250}
+            height={250}
+            placeholder="blur"
+          />
+          <div className="flex justify-center mt-6">
+            <div className="flex md:flex lg:flex space-x-4 font-medium text-orange-800 sm:block dark:text-white">
+              <SocialLinks type="profile" />
+            </div>
+          </div>
+        </div>
         <div className="lg:px-4 lg:mt-12 ">
           <h1 className="text-2xl font-bold text-orange-900 lg:text-5xl dark:text-white">
             Hey, I'm Nitin Reddy.
@@ -28,22 +45,6 @@ const About = () => {
             <p>
               <b>Let's learn, grow and excel together.⚛🚀</b>
             </p>
-          </div>
-        </div>
-        <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
-          <Image
-            src={profile}
-            alt="Profile"
-            priority={true}
-            className="rounded-lg"
-            width={250}
-            height={250}
-            placeholder="blur"
-          />
-          <div className="flex justify-center mt-6">
-            <div className="flex md:flex lg:flex space-x-4 font-medium text-orange-800 sm:block dark:text-white">
-              <SocialLinks type="profile" />
-            </div>
           </div>
         </div>
       </div>

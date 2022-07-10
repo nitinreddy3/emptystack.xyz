476d62fee0f822eeabafa1ad4d3d7b0210172b04
nitin reddy <reddynitin3@gmail.com>
2022-07-10 16:30:06 +0530

    Initial setup

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+
+function MyApp ( { Component, pageProps }: AppProps )
+{
+  return (
+    <ThemeProvider attribute="class" enableSystem={ false }>
+      <Navigation />
+      <Component { ...pageProps } />
+      <Footer />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;

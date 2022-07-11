c781dee2b230668084d1e7fd0e888f9789c43568
nitin reddy <reddynitin3@gmail.com>
2022-07-11 23:27:31 +0530

    Added responsive menus

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,11 +1,46 @@
 import Link from "next/link"
-import React from "react"
+import React, { useState } from "react"
 import ThemeSwitch from "../components/ThemeSwitch"
 
+function MobileNav({ open, setOpen }) {
+  return (
+    <div className={`absolute top-0 left-0 h-screen w-48 bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
+      <div className="flex flex-col items-center">
+        <Link href="/">
+          <a
+            className={"font-medium tracking-wider transition-colors text-emerald-900 hover:text-sky-500 dark:text-white"}
+            onClick={() => setTimeout(() => { setOpen(!open) }, 100)}
+          >
+            About
+          </a>
+        </Link>
+        <Link href="/blog">
+          <a
+            className={" font-medium tracking-wider transition-colors text-emerald-900 hover:text-sky-500 dark:text-white"}
+            onClick={() => setTimeout(() => { setOpen(!open) }, 100)}
+          >
+            Blog
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
 const Navigation = () => {
+  const [open, setOpen] = useState(false)
   return (
     <div className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black">
+      <MobileNav open={open} setOpen={setOpen} />
       <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
+        <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {
+          setOpen(!open)
+        }}>
+          {/* hamburger button */}
+          <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
+          <span className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+          <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
+        </div>
         <Link href="/">
           <a
             className={"font-medium tracking-wider transition-colors text-emerald-900 hover:text-sky-500 uppercase dark:text-white"}
@@ -14,8 +49,7 @@ const Navigation = () => {
           </a>
         </Link>
         <div className="flex items-center">
-          <div className="md:flex lg:flex space-x-4 font-medium text-gray-800 hidden sm:block dark:text-white">
-
+          <div className="navbar md:flex lg:flex space-x-4 font-medium text-gray-800  sm:block dark:text-white">
             <Link href="/">
               <a
                 className={"font-medium tracking-wider transition-colors text-emerald-900 hover:text-sky-500 dark:text-white"}
@@ -31,8 +65,8 @@ const Navigation = () => {
               </a>
             </Link>
           </div>
+          <ThemeSwitch />
         </div>
-        <ThemeSwitch />
       </div>
     </div>
   )

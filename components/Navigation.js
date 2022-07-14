b47c86df7ffc70ebb0bbd6e208eb7aff932c6d6b
nitin reddy <reddynitin3@gmail.com>
2022-07-14 14:03:51 +0530

    Updated the styles

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,7 +4,7 @@ import ThemeSwitch from "../components/ThemeSwitch"
 
 function MobileNav({ open, setOpen }) {
   return (
-    <div className={`absolute top-0 left-0 h-screen w-48 bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
+    <div className={`absolute top-0 left-0 h-screen w-48 bg-white dark:bg-black transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
       <div className="flex flex-col items-center">
         <Link href="/">
           <a
@@ -40,14 +40,14 @@ const Navigation = () => {
   return (
     <div className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black">
       <MobileNav open={open} setOpen={setOpen} />
-      <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
+      <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between ">
         <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {
           setOpen(!open)
         }}>
           {/* hamburger button */}
-          <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
-          <span className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
-          <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
+          <span className={`h-1 w-full bg-emerald-900 dark:bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
+          <span className={`h-1 w-full bg-emerald-900 dark:bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+          <span className={`h-1 w-full bg-emerald-900 dark:bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
         </div>
         <Link href="/">
           <a

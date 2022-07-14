74ac8e305b112027ac6a1efb340deb0c96c8f2d3
nitin reddy <reddynitin3@gmail.com>
2022-07-14 09:41:38 +0530

    Added meta tags

--- next.config.js
+++ next.config.js
@@ -3,14 +3,5 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com', 'media-exp1.licdn.com', '19yw4b240vb03ws8qm25h366-wpengine.netdna-ssl.com', 'marvelapp.com'],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/about',
-        destination: '/',
-        permanent: true, // triggers 308
-      },
-    ];
-  },
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,13 @@ export default function Home ()
         <title>EmptyStack</title>
         <link rel="icon" href="/code.png" />
         <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+        <meta name="robots" content="all" />
+        <meta name="googlebot" content="all" />
+        <link
+          rel="canonical"
+          href="https://emptystack.dev"
+          key="canonical"
+        />
       </Head>
 
       <main className="max-w-4xl mx-auto mt-16 antialiased">

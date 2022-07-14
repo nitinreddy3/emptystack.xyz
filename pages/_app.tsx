49fa29cbe607b4f72054dba8ae403dbd3120106d
nitin reddy <reddynitin3@gmail.com>
2022-07-14 13:10:38 +0530

    Fixed the issue with tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
@@ -7,25 +6,6 @@ import Footer from "../components/Footer";
 
 function MyApp ( { Component, pageProps }: AppProps )
 {
-  useEffect( () =>
-  {
-    if ( "serviceWorker" in navigator )
-    {
-      window.addEventListener( "load", function ()
-      {
-        navigator.serviceWorker.register( "/sw.js" ).then(
-          function ( registration )
-          {
-            console.log( "Service Worker registration successful with scope: ", registration.scope );
-          },
-          function ( err )
-          {
-            console.log( "Service Worker registration failed: ", err );
-          }
-        );
-      } );
-    }
-  }, [] );
   return (
     <ThemeProvider attribute="class" enableSystem={ false }>
       <Navigation />
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,7 @@ module.exports = {
   },
   plugins: [],
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
+  darkMode: 'class',
   variants: {
     extend: {},
   },

148004774cc2a317b7a8148d882fbd33e96b8112
nitin reddy <reddynitin3@gmail.com>
2022-07-14 12:55:46 +0530

    Added service worker file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
@@ -6,6 +7,25 @@ import Footer from "../components/Footer";
 
 function MyApp ( { Component, pageProps }: AppProps )
 {
+  useEffect( () =>
+  {
+    if ( "serviceWorker" in navigator )
+    {
+      window.addEventListener( "load", function ()
+      {
+        navigator.serviceWorker.register( "/sw.js" ).then(
+          function ( registration )
+          {
+            console.log( "Service Worker registration successful with scope: ", registration.scope );
+          },
+          function ( err )
+          {
+            console.log( "Service Worker registration failed: ", err );
+          }
+        );
+      } );
+    }
+  }, [] );
   return (
     <ThemeProvider attribute="class" enableSystem={ false }>
       <Navigation />

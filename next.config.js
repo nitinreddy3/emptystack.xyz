13706f5a6ccb3bfc8706b1eb86f0b7ef54311757
nitin reddy <reddynitin3@gmail.com>
2022-07-14 10:32:44 +0530

    Added alt tags to the social links

--- next.config.js
+++ next.config.js
@@ -5,5 +5,9 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com', 'media-exp1.licdn.com', '19yw4b240vb03ws8qm25h366-wpengine.netdna-ssl.com', 'marvelapp.com'],
-  }
+  },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 }

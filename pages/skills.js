b932327b7caac2d51f7375e80fe1f57c30364e71
nitin reddy <reddynitin3@gmail.com>
2022-08-14 23:45:33 +0530

    Refactored the code for the skills section

--- pages/skills.js
+++ pages/skills.js
@@ -1,17 +1,51 @@
 import React from "react"
+import 'chart.js/auto';
+import { Pie } from 'react-chartjs-2';
+
+const data = {
+  labels: [
+    'JavaScript',
+    'React',
+    'Node',
+    'Python',
+    'Others'
+  ],
+  datasets: [{
+    data: [50, 50, 50, 50, 50],
+    backgroundColor: [
+      '#f2aa0e',
+      '#00ffa1',
+      '#36A2EB',
+      '#FFCE56',
+      '#0e7cf2'
+    ],
+    hoverBackgroundColor: [
+      '#f2aa0e',
+      '#00ffa1',
+      '#36A2EB',
+      '#FFCE56',
+      '#0e7cf2',
+
+    ]
+  }]
+}
 
 const Skills = () => {
   return (
-    <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
+    <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-center">
       <div className="lg:space-x-12 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
         <div className="lg:mt-12">
           <h1 className="text-center text-2xl font-bold text-emerald-900 lg:text-5xl dark:text-white">
             Skills
           </h1>
           <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-            <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-              Coming soon...💣🚣
-            </div>
+
+            <Pie
+              data={data}
+              // width={400}
+              // height={400}
+              style={{ width: '30vw', height: '30vh' }}
+            />
           </div>
         </div>
       </div>

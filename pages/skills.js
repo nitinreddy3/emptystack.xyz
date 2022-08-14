fc45cdc981873f764e4ef7b73ff35222d0455a37
nitin reddy <reddynitin3@gmail.com>
2022-08-14 23:16:59 +0530

    Added the skill set component

---
+++ pages/skills.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Skills = () => {
+  return (
+    <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
+      <div className="lg:space-x-12 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
+        <div className="lg:mt-12">
+          <h1 className="text-center text-2xl font-bold text-emerald-900 lg:text-5xl dark:text-white">
+            Skills
+          </h1>
+          <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+            <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills;

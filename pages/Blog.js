4866af07dab2f6ee01bf1fddb3bc566c53acbe74
nitin reddy <reddynitin3@gmail.com>
2022-08-15 23:50:43 +0530

    Updated the blog page

--- pages/Blog.js
+++ pages/Blog.js
@@ -39,23 +39,21 @@ const Blog = () => {
             Blogs
           </h1>
           <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full text-emerald-800 dark:text-white">
-            <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-              {data.length ? showPosts() :
-                <div className="animate-pulse flex space-x-4">
-                  <div className="rounded-full bg-slate-700 h-10 w-10"></div>
-                  <div className="flex-1 space-y-6 py-1">
-                    <div className="h-2 bg-slate-700 rounded"></div>
-                    <div className="space-y-3">
-                      <div className="grid grid-cols-3 gap-4">
-                        <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                        <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                      </div>
-                      <div className="h-2 bg-slate-700 rounded"></div>
+            {data.length ? showPosts() :
+              <div className="animate-pulse flex space-x-4">
+                <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+                <div className="flex-1 space-y-6 py-1">
+                  <div className="h-2 bg-slate-700 rounded"></div>
+                  <div className="space-y-3">
+                    <div className="grid grid-cols-3 gap-4">
+                      <div className="h-2 bg-slate-700 rounded col-span-2"></div>
+                      <div className="h-2 bg-slate-700 rounded col-span-1"></div>
                     </div>
+                    <div className="h-2 bg-slate-700 rounded"></div>
                   </div>
                 </div>
-              }
-            </div>
+              </div>
+            }
           </div>
         </div>
       </div>
